01967aec9e44878c927ebca3c73ba50736ba511a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-15 16:15:03 -0600

    Initial Commit

---
+++ BarcodeScannerSA/BarcodeScannerSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerSAApp.swift
+//  BarcodeScannerSA
+//
+//  Created by Mitch Andrade on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
