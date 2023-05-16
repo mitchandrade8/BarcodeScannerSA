616b739131c786794f4c58bf34a02de4e6ec4949
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-16 09:35:22 -0600

    Start of errors and alerts for barcode scanner

--- BarcodeScannerSA/BarcodeScannerView.swift
+++ BarcodeScannerSA/BarcodeScannerView.swift
@@ -7,9 +7,28 @@
 
 import SwiftUI
 
+struct AlertItem {
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(
+        title: "Invalid Device Input",
+        message: "Something is wrong with the camera. We are unable to capture the input.",
+        dismissButton: .default(Text("OK")))
+    
+    static let invalidScannedType = AlertItem(
+        title: "Invalid Scan Type",
+        message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
+        dismissButton: .default(Text("OK")))
+}
+
 struct BarcodeScannerView: View {
     
     @State private var scannedCode = ""
+    @State private var alertItem: AlertItem?
     
     var body: some View {
         NavigationView {
@@ -27,8 +46,10 @@ struct BarcodeScannerView: View {
                     .font(.largeTitle)
                     .foregroundColor(scannedCode.isEmpty ? .red : .green)
                     .padding()
+                
             }
             .navigationTitle("Barcode Scanner")
+           
         }
     }
 }
