ddbfa2d9054bc3820080c473f15aa067879e3b1a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-16 00:11:58 -0600

    Intro & UI Setup (11:27)

--- BarcodeScannerSA/BarcodeScannerSAApp.swift
+++ BarcodeScannerSA/BarcodeScannerSAApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BarcodeScannerSAApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BarcodeScannerView()
         }
     }
 }
